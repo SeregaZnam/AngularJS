1b6106c4c34fa9360187591a7ddbca5fb2aac35a
SeregaZnam <Sergey-znamensk@yandex.ru>
2018-08-16 22:05:44 +0400

    Анимация

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 (function(){
 
-	var app = angular.module('MyApp', ['ui.router']);
+	var app = angular.module('MyApp', ['ui.router', 'ngAnimate']);
 
 	app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
 
-		$urlRouterProvider.otherwise('/404');
+		$urlRouterProvider.otherwise('/#');
 
 		$stateProvider
 			.state('home', {
@@ -18,27 +18,7 @@
 				url: '/book/:id',
 				templateUrl: 'templates/book.html',
 				controller: 'BookController',
-			})
-			.state('404', {
-				url: '/404',
-				templateUrl: 'templates/404.html'
 			});
-
-		/*$routeProvider
-			.when('/', {
-				controller: 'BooksController',
-				templateUrl: 'templates/books.html'
-			})
-			.when('/book/:id', {
-				controller: 'BookController',
-				templateUrl: 'templates/book.html'
-			})
-			.when('/404', {
-				templateUrl: 'templates/404.html'
-			})
-			.otherwise({
-				redirectTo: '/404'
-			});*/
 	}]);
 
 }());
